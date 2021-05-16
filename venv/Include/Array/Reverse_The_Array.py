4e2c65c31162ca301c012b2d916070ca85750dcf
Krishna Kant Sharma <kksharmaz786@gmail.com>
2021-05-16 11:44:10 +0530

    Reverse using Indexing completed

--- venv/Include/Array/Reverse_The_Array.py
+++ venv/Include/Array/Reverse_The_Array.py
@@ -1,7 +1,9 @@
-#           Ussing reversed method
+# ...................Using reversed method..........................
 def Reverse_Using_Reversed(arr):
     return list(reversed(arr))
 
+
+# .....................Using swap ..................................
 def Reverse_Using_Swap(arr):
     i = 0
     j = len(arr) - 1
@@ -11,6 +13,13 @@ def Reverse_Using_Swap(arr):
         j = j - 1
     return arr
 
-arr = list(map(int,input().split()))
+
+# .....................Using indexing..................
+def Reverse_Using_Indexing(arr):
+    return arr[::-1]
+
+
+arr = list(map(int, input().split()))
 print("Using Reversed: ", Reverse_Using_Reversed(arr))
+print("Using Indexing: ", Reverse_Using_Indexing(arr))
 print("Using Swap: ", Reverse_Using_Swap(arr))
