5917cb227ec1659a72a1393f773d95fd225d2839
Krishna Kant Sharma <kksharmaz786@gmail.com>
2021-05-16 11:38:19 +0530

    Reverse using swap completed

--- venv/Include/Array/Reverse_The_Array.py
+++ venv/Include/Array/Reverse_The_Array.py
@@ -2,8 +2,15 @@
 def Reverse_Using_Reversed(arr):
     return list(reversed(arr))
 
+def Reverse_Using_Swap(arr):
+    i = 0
+    j = len(arr) - 1
+    while i < j:
+        arr[i], arr[j] = arr[j], arr[i]
+        i = i + 1
+        j = j - 1
+    return arr
 
 arr = list(map(int,input().split()))
-print("Using Reversed: ",Reverse_Using_Reversed(arr))
-
-
+print("Using Reversed: ", Reverse_Using_Reversed(arr))
+print("Using Swap: ", Reverse_Using_Swap(arr))
