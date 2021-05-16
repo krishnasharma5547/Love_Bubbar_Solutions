4c934392cd8a7a0f82159ed7d9286e33fe79b60d
Krishna Kant Sharma <kksharmaz786@gmail.com>
2021-05-16 12:26:04 +0530

    Using Linear traverse method Done

--- venv/Include/Array/Find the maximum and minimum element in an array.py
+++ venv/Include/Array/Find the maximum and minimum element in an array.py
@@ -3,6 +3,16 @@ def Using_Max_Min(arr):
     mini = min(arr)
     return "minimum = " + str(mini) + " maximun= " + str(maxi)
 
+def Using_Linear_Traverse(arr):
+    mini = 999999
+    maxi = -9999999
+    for i in arr:
+        if i < mini:
+            mini = i
+        if i > maxi:
+            maxi = i
+    return "minimum = " + str(mini) + " maximun= " + str(maxi)
 
 arr = list(map(int, input().split()))
 print(Using_Max_Min(arr))
+print(Using_Linear_Traverse(arr))
